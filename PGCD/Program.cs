37ce79f9646591b0c996c638653a0af314383a2c
Alex <61283644+KafaloAlex@users.noreply.github.com>
2021-01-04 06:13:32 +0000

    Ajout du programme

---
+++ PGCD/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PGCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calcul du PGCD de deux nombres (Méthode d'Euclide)";
+
+            Console.Write("Entrez le premier nombre : ");
+            var nbre1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Entrez le second nombre : ");
+            var nbre2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("pgcd({0};{1}) = {2}", nbre1, nbre2, pgcd(nbre1, nbre2));
+        }
+
+        static int pgcd(int x, int y)
+        {
+            if (x < y)
+            {
+                int r;
+                do
+                {
+                    r = x % y;
+                    x = y;
+                    y = r;
+                } while (r != 0);
+                return x;
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible - Valeur Nulle");
+                return 0;
+            }
+        }
+    }
+}
